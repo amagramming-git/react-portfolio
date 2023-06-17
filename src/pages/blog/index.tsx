15f2892b2e896dbe9e942a7f9df175e8dfa9ae03
amagramming-git <knb1031_ba@icloud.com>
2023-06-17 16:13:34 +0900

    styled-componentsを追加 npm i @types/styled-components@latestを実行してインストール。画面を開いたときに一瞬適用されない問題発生

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,14 +1,54 @@
 import { useRouter } from "next/router"
+import styled from "styled-components";
+
+
+/* POINT 生成する要素を指定し、スタイルをテンプレートリテラルで記述します */
+// React要素扱いなので変数名は大文字で記述！
+const StyledButton = styled.button`
+  margin-inline: auto;
+  border-radius: 9999px;
+  border: none;
+  display: block;
+  width: 120px;
+  height: 60px;
+  margin: 10px auto;
+  font-weight: bold;
+  cursor: pointer;
+  text-align: center;
+  line-height: 60px;
+  transition: all 0.3s ease-out;
+
+  /* POINT 疑似クラスの追加 */
+  :hover,
+  :active {
+    opacity: 0.7;
+    transform: scale(1.1);
+  }
+  span {
+    color: purple;
+  }
+  /* POINT メディアクエリ */
+  @media (max-width: 600px) {
+    border-radius: 0;
+  }
+
+  :global {
+    background-color: black;
+  }
+`;
+
 
 export default function Home() {
-    const router = useRouter();
-    const clickHandler = () => {
-        router.push('/')
-    }
-    return (
-        <>
-            <h1>Home</h1>
-            <button onClick={clickHandler}>アクションに夜画面遷移</button>
-        </>
-    )
+  const router = useRouter();
+  const onClickHandler = () => {
+    router.push('/')
+  }
+  return (
+    <>
+      <h1>Home</h1>
+      <StyledButton onClick={onClickHandler}>
+        ボタン
+      </StyledButton>
+    </>
+  )
 }
